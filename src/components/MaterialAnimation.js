66c140560a16e11b019a12c8c8a82d43d39bc85e
Vakul16 <valusavakhul@gmail.com>
2021-08-18 23:48:29 +0530

    Worked on Box layout

--- src/components/MaterialAnimation.js
+++ src/components/MaterialAnimation.js
@@ -4,6 +4,9 @@ import Paper from "@material-ui/core/Paper";
 import Fade from "@material-ui/core/Fade";
 import ReviewTrend from "views/ReviewManagement/Components/snapshot/ReviewTrend";
 import BoxLayout from "views/ReviewManagement/Components/Layouts/BoxLayout";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 const useStyles = makeStyles((theme) => ({
   //   root: {
   //     height: 180,
@@ -13,12 +16,26 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
   },
+  expandMore: {
+    color: "#fff",
+  },
+  expandLess: { color: "#fff" },
+  arrow: {
+    // display: "flex",
+    position: "absolute",
+    top: "180px",
+    right: "360px",
+  },
 }));
 
 export default function MaterialAnimation() {
   const classes = useStyles();
   //   const [checked, setChecked] = React.useState(false);
   const [offsetY, setOffsetY] = useState(0);
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
   const handleScroll = () => setOffsetY(window.pageYOffset);
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -72,10 +89,21 @@ export default function MaterialAnimation() {
         <Fade in={setOffsetY} timeout={0}>
           <Paper elevation={0}>
             <BoxLayout label="Review Trends">
-              <ReviewTrend
-                filterData={radioData}
-                setFilterSelected={handleRadioChange}
-              />
+              <div button onClick={handleClick} className={classes.arrow}>
+                {open ? (
+                  <ExpandLess className={classes.expandLess} />
+                ) : (
+                  <ExpandMore className={classes.expandMore} />
+                )}
+              </div>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <div component="div" disablePadding>
+                  <ReviewTrend
+                    filterData={radioData}
+                    setFilterSelected={handleRadioChange}
+                  />
+                </div>
+              </Collapse>
             </BoxLayout>
           </Paper>
         </Fade>
